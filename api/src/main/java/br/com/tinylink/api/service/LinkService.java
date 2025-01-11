821eea1dd52b00ade2f2a205ed514e72d75cf52d
Claudio Rocha <contatojhonfk@gmail.com>
2025-01-10 21:42:19 -0300

    Implementada inversão de dependências no LinkService

--- api/src/main/java/br/com/tinylink/api/service/LinkService.java
+++ api/src/main/java/br/com/tinylink/api/service/LinkService.java
@@ -7,37 +7,8 @@ import org.springframework.stereotype.Service;
 import br.com.tinylink.api.models.Link;
 import br.com.tinylink.api.repository.LinkRepository;
 
-@Service
-public class LinkService {
-    
-    @Autowired
-    private LinkRepository linkRepository;
-
-    public boolean add(Link link){
-
-        try{
-            linkRepository.save(link);
-            return true;
-            
-        } catch(Exception e){
-            return false;
-        }
-
-    }
-
-    public List<Link> list(){
-        return linkRepository.findAll();
-    }
-
-    public Link findLinkByCode(Integer code){
-        ArrayList<Link> links = new ArrayList<>( list() );
-        for(Link link : links){
-            if(link.getCode().equals(code)){
-                System.out.println("entrei");
-                return link;
-            }
-        }
-        return null;
-
-    }
+public interface LinkService {
+    boolean add(Link link);
+    List<Link> list();
+    Link findLinkByCode(Integer code);
 }
---
+++ api/src/main/java/br/com/tinylink/api/service/LinkServiceImpl.java
@@ -0,0 +1,44 @@
+package br.com.tinylink.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.tinylink.api.models.Link;
+import br.com.tinylink.api.repository.LinkRepository;
+
+@Service
+public class LinkServiceImpl implements LinkService {
+
+    @Autowired
+    private LinkRepository linkRepository;
+
+    public boolean add(Link link){
+
+        try{
+            linkRepository.save(link);
+            return true;
+            
+        } catch(Exception e){
+            return false;
+        }
+
+    }
+
+    public List<Link> list(){
+        return linkRepository.findAll();
+    }
+
+    public Link findLinkByCode(Integer code){
+        ArrayList<Link> links = new ArrayList<>( list() );
+        for(Link link : links){
+            if(link.getCode().equals(code)){
+                System.out.println("entrei");
+                return link;
+            }
+        }
+        return null;
+    }
+}
